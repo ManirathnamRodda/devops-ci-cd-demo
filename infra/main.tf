fb5dffb0e8b61e5c264a6c19cce586285a356629
ManirathnamRodda <manirathnam987@gmail.com>
2025-07-17 15:49:29 +0530

    Add GitHub Action to push Docker image to ECR

--- infra/main.tf
+++ infra/main.tf
@@ -1,20 +1,65 @@
-resource "aws_eks_cluster" "main" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster_role.arn
-
-  vpc_config {
-    subnet_ids = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_AmazonEKSClusterPolicy
-  ]
+provider "aws" {
+  region = var.region
 }
 
-# IAM role for EKS
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "eksClusterRole"
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name = "devops-vpc"
+  }
+}
 
+# Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Create public subnets
+resource "aws_subnet" "subnet1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "devops-subnet-1"
+  }
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "devops-subnet-2"
+  }
+}
+
+# Route table and association
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "subnet1_assoc" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_route_table_association" "subnet2_assoc" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.rt.id
+}
+
+# IAM Role for EKS Control Plane
+resource "aws_iam_role" "eks_role" {
+  name = "eks-cluster-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
@@ -27,24 +72,71 @@ resource "aws_iam_role" "eks_cluster_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "eks_AmazonEKSClusterPolicy" {
+resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_cluster_role.name
+  role       = aws_iam_role.eks_role.name
 }
 
-# Minimal Subnet (for demo)
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+# IAM Role for EKS Worker Nodes
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
 }
 
-resource "aws_subnet" "subnet1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+resource "aws_iam_role_policy_attachment" "node_policy_eks_worker" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
 }
 
-resource "aws_subnet" "subnet2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
+resource "aws_iam_role_policy_attachment" "node_policy_ec2_container" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_policy_cni" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+# EKS Cluster
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_policy_attachment]
+}
+
+# EKS Node Group
+resource "aws_eks_node_group" "devops_node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "devops-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+  instance_types  = ["t3.medium"]
+  scaling_config {
+    desired_size = 2
+    max_size     = 2
+    min_size     = 1
+  }
+
+  force_update_version = true
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node_policy_eks_worker,
+    aws_iam_role_policy_attachment.node_policy_ec2_container,
+    aws_iam_role_policy_attachment.node_policy_cni
+  ]
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,9 +1,7 @@
-variable "aws_region" {
-  description = "AWS region"
-  default     = "us-east-1"
+variable "region" {
+  default = "us-east-1"
 }
 
 variable "cluster_name" {
-  description = "EKS Cluster name"
-  default     = "devops-ci-cd-cluster"
+  default = "devops-ci-cd-cluster"
 }
