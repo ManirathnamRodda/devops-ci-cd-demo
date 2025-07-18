7462b6a306614fe840f087645fcf96f091260968
ManirathnamRodda <manirathnam987@gmail.com>
2025-07-18 12:33:59 +0530

    code changes completed

--- app/app.js
+++ app/app.js
@@ -3,10 +3,14 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+<<<<<<< HEAD
   res.send(`🚀 Hello from DevOps CI/CD Demo App! Welcome to my world`);
 });
 
 
+=======
+  res.send('🚀 Hello from DevOps CI/CD Demo App! Welocme to my world');
+>>>>>>> 8c0c7d7 (code changes completed)
 });
 
 const PORT = process.env.PORT || 3000;
