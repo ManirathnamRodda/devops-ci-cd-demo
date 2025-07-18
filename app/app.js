980854914a91b291d68ed39659d7fe527dbefb34
ManirathnamRodda <manirathnam987@gmail.com>
2025-07-18 14:05:25 +0530

    new change2

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send(`🚀 Hello from DevOps CI/CD Demo App! Welcome to my worlds`);
+  res.send(`🚀 Hello from DevOps CI/CD Demo App! Welcome Mani to the Devops worlds`);
 });
 
 const PORT = process.env.PORT || 3000;
